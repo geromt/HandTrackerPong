63097872794ee3b515614a9f8d183c1e0211b397
gero <gerardomartinez205@gmail.com>
2023-08-14 22:34:59 -0600

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public UDPReceiver udpReceiver;
+    public float moveSpeed;
+    public GameObject leftPlayer;
+    public GameObject rightPlayer;
+
+    void Update()
+    {
+        var heights = ReadTrackerData();
+    }
+
+    private (float leftHeight, float rightHeight) ReadTrackerData()
+    {
+        string data = udpReceiver.data;
+        data = data.Remove(0, 1);
+        data = data.Remove(data.Length-1, 1);
+        string[] heights = data.Split(',');
+
+        float leftHeight = float.Parse(heights[0]);
+        float rightHeight = float.Parse(heights[1]);
+
+        return (leftHeight, rightHeight);
+    }
+
+    private void MovePlayer(GameObject player, float height)
+    {
+        
+    }
+}
---
+++ Assets/Scripts/WebCamController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebCamController : MonoBehaviour
+{
+	public MeshRenderer webCamRenderer;
+	
+	private WebCamDevice[] devices;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        devices = WebCamTexture.devices;
+        foreach (WebCamDevice device in devices)
+        {
+        	Debug.Log(device.name);
+        }
+
+        WebCamTexture webCamTexture = new WebCamTexture(devices[0].name);
+        webCamRenderer.material.mainTexture = webCamTexture;
+        webCamTexture.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
